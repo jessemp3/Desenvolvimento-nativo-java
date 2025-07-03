53ea28055d4827be71d8b31391b43dd592d7aeb2
jesse <nitchsx@gmail.com>
2025-07-03 01:53:11 -0300

    feat: rename Exe1 and Exe2 classes to Exe1Activity and Exe2Activity, update layout files and colors for improved consistency

--- LinearLayout/app/src/main/java/com/nativo/linearlayout/Exe1Activity.java
+++ LinearLayout/app/src/main/java/com/nativo/linearlayout/Exe1Activity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Exe1 extends AppCompatActivity {
+public class Exe1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- LinearLayout/app/src/main/java/com/nativo/linearlayout/Exe2Activity.java
+++ LinearLayout/app/src/main/java/com/nativo/linearlayout/Exe2Activity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Exe2 extends AppCompatActivity {
+public class Exe2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
