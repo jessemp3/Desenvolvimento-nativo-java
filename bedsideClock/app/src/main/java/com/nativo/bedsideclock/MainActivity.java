cba719a92dbfb6c987b0796133b9737713270fd4
jesse <nitchsx@gmail.com>
2025-07-31 01:53:48 -0300

    feat: add battery level display and settings descriptions in main activity

--- bedsideClock/app/src/main/java/com/nativo/bedsideclock/MainActivity.java
+++ bedsideClock/app/src/main/java/com/nativo/bedsideclock/MainActivity.java
@@ -1,6 +1,13 @@
 package com.nativo.bedsideclock;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.nativo.bedsideclock.databinding.ActivityMainBinding;
+
+// 1 - bateria
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private final BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            binding.textViewBattery.setText(level + "%");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // flag de manter a tela ligada
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); // essa flag toma a tela inteira e some com a barra de status
+
     }
+    //broadcast
+
 }
