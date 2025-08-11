0d61e836370340d303d1617df9a97f12c29891d9
jesse <nitchsx@gmail.com>
2025-08-11 16:08:23 -0300

    feat: add initial project files including MainActivity, layout, and resource files

---
+++ weather_project/app/src/main/java/com/nativo/weather_project/MainActivity.java
@@ -0,0 +1,26 @@
+package com.nativo.weather_project;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+
+// https://chatgpt.com/share/68894acd-bc94-8007-9e8e-889a5fc17909
