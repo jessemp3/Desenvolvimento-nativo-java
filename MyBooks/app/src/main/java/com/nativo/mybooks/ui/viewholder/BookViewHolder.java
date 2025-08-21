e3a6e7c86e1ccd6594cc6baad6e565315e1dce29
jesse <nitchsx@gmail.com>
2025-08-21 04:41:04 -0300

    feat: enhance BookViewHolder with genre display and favorite icon functionality

--- MyBooks/app/src/main/java/com/nativo/mybooks/ui/viewholder/BookViewHolder.java
+++ MyBooks/app/src/main/java/com/nativo/mybooks/ui/viewholder/BookViewHolder.java
@@ -3,6 +3,7 @@ package com.nativo.mybooks.ui.viewholder;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nativo.mybooks.R;
 import com.nativo.mybooks.databinding.ItemBookBinding;
 import com.nativo.mybooks.entity.BookEntity;
 
@@ -14,7 +15,30 @@ public class BookViewHolder extends RecyclerView.ViewHolder {
         item = itemView;
     }
 
-    public void bind(BookEntity book){
+    public void bind(BookEntity book) {
         item.textViewTitle.setText(book.getTitle());
+        item.textViewGenre.setText(book.getGenre());
+        item.textViewAuthor.setText(book.getAuthor());
+        updateFavoriteIcon(book);
+        setGenereBackgroundColor(book);
     }
+
+    public void updateFavoriteIcon(BookEntity book) {
+        if (book.isFavorite()) {
+            item.imageViewFavorite.setImageResource(R.drawable.ic_favorite);
+        } else {
+            item.imageViewFavorite.setImageResource(R.drawable.ic_favorite_empty);
+        }
+    }
+
+    private void setGenereBackgroundColor(BookEntity book) {
+        if (book.getGenre().equals("Fantasia")) {
+            item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy);
+        } else if (book.getGenre().equals("Terror")) {
+            item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_red);
+        }else{
+            item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_teal);
+        }
+    }
+
 }
